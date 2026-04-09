d1281b4240de7fe23a835528cea991556a355fc2
Thanh Phong Le <ltphongssvn@gmail.com>
2026-04-09 15:52:34 +0000

    style: blank line between wandb and first-party import in log_quality_signals

--- src/wandb_logger.py
+++ src/wandb_logger.py
@@ -112,6 +112,7 @@ def log_quality_signals(
 ) -> dict[str, Any]:
     """Log ModelQualitySignals summary to the active W&B run."""
     import wandb
+
     from src.dataset_probe import ModelQualitySignals
 
     signal_counts: dict[str, int] = {}
